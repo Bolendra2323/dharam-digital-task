0d480bc390e412539655589e7126f676639db5b5
Bolendra -- Uranium <bolendra10@gmail.com>
2022-12-03 10:43:59 +0530

    Update

--- src/controller/redirect.js
+++ src/controller/redirect.js
@@ -20,15 +20,30 @@ const redirectURL = async function(req, res, next) {
             return res.status(404).send({ status: false, message: "Campaign Does not exist" })
         }
         if (campaignPresent) {
-            const url = ["http://google.com/?click=egwkeuyeurcdkvdi3", "http://microsoft.com/?msclickid=egwkeuyeurcdkvdi3"];
-            const a = Array(30).fill(url[0]);
-            const b = Array(70).fill(url[1]);
-            if (a) {
-                res.redirect(301, a);
-                next();
+            var requestCounter = 0;
+
+            // Increment the request counter
+            requestCounter++;
+
+            // Check if the request counter is less than or equal to 30
+            if (requestCounter <= 30) {
+                // Redirect to the first URL
+                res.redirect('"http://google.com/?click=egwkeuyeurcdkvdi3');
             } else {
-                res.redirect(301, b);
+                // Redirect to the second URL
+                res.redirect('http://microsoft.com/?msclickid=egwkeuyeurcdkvdi3');
             }
+
+
+            // const url = ["http://google.com/?click=egwkeuyeurcdkvdi3", "http://microsoft.com/?msclickid=egwkeuyeurcdkvdi3"];
+            // const a = Array(30).fill(url[0]);
+            // const b = Array(70).fill(url[1]);
+            // if (a) {
+            //     res.redirect(301, a);
+            //     next();
+            // } else {
+            //     res.redirect(301, b);
+            // }
 
         }
     } catch (err) {
